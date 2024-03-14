d05aa8e51fa1c10c82e33f77fe96a5f08a2e19c2
IC-huanglc <huanglc_work@163.com>
2024-03-14 11:04:34 +0800

    add file.f

---
+++ amba_ahb/vcs/file.f
@@ -0,0 +1,11 @@
+/home/ICer/xilinx_prj/amba_ahb/vcs/srcs/ahb_arbiter_m2.v 
+/home/ICer/xilinx_prj/amba_ahb/vcs/srcs/ahb_decoder_s3.v 
+/home/ICer/xilinx_prj/amba_ahb/vcs/srcs/ahb_default_slave.v 
+/home/ICer/xilinx_prj/amba_ahb/vcs/srcs/ahb_lite_s3.v 
+/home/ICer/xilinx_prj/amba_ahb/vcs/srcs/ahb_m2s_m2.v 
+/home/ICer/xilinx_prj/amba_ahb/vcs/srcs/ahb_s2m_s3.v 
+/home/ICer/xilinx_prj/amba_ahb/vcs/srcs/amba_ahb_m2s3.v 
+/home/ICer/xilinx_prj/amba_ahb/vcs/srcs/bfm_ahb.v 
+/home/ICer/xilinx_prj/amba_ahb/vcs/srcs/mem_ahb.v 
+/home/ICer/xilinx_prj/amba_ahb/vcs/srcs/top.v 
+
